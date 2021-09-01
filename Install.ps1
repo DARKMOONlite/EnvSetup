1cbd514d621b701267643f1008e2a3cbbbe00ad0
Albert Ferguson <albertferguson118@gmail.com>
2021-09-01 19:43:51 +1000

    'updated'

--- Install.ps1
+++ Install.ps1
@@ -1,5 +1,12 @@
 if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit }
 
+function InstallApps([string[]]$Apps)
+{
+    foreach ($app in $Apps) {
+        choco install $app -y
+    }
+}
+
 function Check-Command($cmdname) {
     return [bool](Get-Command -Name $cmdname -ErrorAction SilentlyContinue)
 }
@@ -12,24 +19,25 @@ Rename-Computer -NewName $computerName
 Write-Host ""
 Write-Host "Disable Sleep on AC Power..." -ForegroundColor Green
 Write-Host "------------------------------------" -ForegroundColor Green
-Powercfg /Change monitor-timeout-ac 20
-Powercfg /Change standby-timeout-ac 0
+Powercfg /Change monitor-timeout-ac 15
+Powercfg /Change standby-timeout-ac 45
 
 # To list all appx packages:
 # Get-AppxPackage | Format-Table -Property Name,Version,PackageFullName
 Write-Host "Removing UWP Rubbish..." -ForegroundColor Green
 Write-Host "------------------------------------" -ForegroundColor Green
 $uwpRubbishApps = @(
-    "Microsoft.Messaging",
-    "king.com.CandyCrushSaga",
-    "Microsoft.BingNews",
-    "Microsoft.MicrosoftSolitaireCollection",
-    "Microsoft.People",
-    "Microsoft.WindowsFeedbackHub",
-    "Microsoft.YourPhone",
-    "Microsoft.MicrosoftOfficeHub",
-    "Fitbit.FitbitCoach",
-    "Microsoft.GetHelp")
+    "Microsoft.Messaging"
+    "king.com.CandyCrushSaga"
+    "Microsoft.BingNews"
+    "Microsoft.MicrosoftSolitaireCollection"
+    "Microsoft.People"
+    "Microsoft.WindowsFeedbackHub"
+    "Microsoft.YourPhone"
+    "Microsoft.MicrosoftOfficeHub"
+    "Fitbit.FitbitCoach"
+    "Microsoft.GetHelp"
+    )
 
 foreach ($uwp in $uwpRubbishApps) {
     Get-AppxPackage -Name $uwp | Remove-AppxPackage
@@ -45,7 +53,7 @@ else {
     Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 }
 
-Write-Host "Config console aesthetics." -ForegroundColor Green
+Write-Host "Config Firacode aesthetics." -ForegroundColor Green
 Write-Host "------------------------------------" -ForegroundColor Green
 choco install "firacode" -y
 
@@ -58,47 +66,114 @@ Write-Host "Console aesthetics have been updated for the cmd. Please restart any
 Write-Host ""
 Write-Host "Installing Applications..." -ForegroundColor Green
 Write-Host "------------------------------------" -ForegroundColor Green
-Write-Host "[WARN] Ma de in China: some software like Google Chrome require the true Internet first" -ForegroundColor Yellow
+Write-Host "[WARN] If in China: some software e.g. Chrome will require unobstricted internet to download!" -ForegroundColor Yellow
 
-$Apps = @(
-    "7zip.install",
-    "git",
-    "vscode",
-    "doxygen.install",
-    "putty",
-    "pingplotter",
-    "wireshark",
-    "zotero",
-    "r.studio",
-    "r",
-    "windirstat",
-    "gimp",
-    "samsung-magician",
-    "sublimetext3.app",
-    "python",
-    "python2",
-    "microsoft-edge",
-    "googlechrome",
-    "wget",
-    "openssl.light",
-    "vscode",
-    "microsoft-teams.install",
-    "github-desktop",
-    "vlc",
-    "steam",
-    "qbittorrent",
-    "msiafterburner",
+
+# Choose your editors here
+$editors = @(
+    "vscode"
+    "sublimetext3.app"
+    "visualstudio2019community"
     )
 
-foreach ($app in $Apps) {
-    choco install $app -y
-}
 
-Write-Host "Beginning Python and R Dev Env set up." -ForegroundColor Green
+# utilities for system testing and general use
+$pcSystemApps = @(
+    "msiafterburner"
+    "7zip.install"
+    "samsung-magician"
+    "icue"
+    "discord"
+    "ddu"
+    "msiafterburner"
+    "rufus"
+    "hwinfo"
+    )
+
+
+# development utilies
+$utilities = @(
+    "git"
+    "doxygen.install"
+    "putty"
+    "pingplotter"
+    "wireshark"
+    "windirstat"
+    "wget"
+    "openssl.light"
+    "hfsexplorer"
+    )
+
+
+# university, documents, admin stuff
+$productivity = @(
+    "zotero"
+    "gimp"
+    "microsoft-teams.install"
+    "grammarly-edge"
+    "grammarly"
+    "adobereader"
+    "parsec"
+    )
+
+
+# I use pretty much everything,
+# Note: vc runtimes are for all since 2005!!!
+#   If you have any issues uninstall all but 2017/19
+$langsAndRuntimes = @(
+    "r"
+    "python"
+    "nodejs"
+    "jre8"
+    "vcredist-all"
+    )
+
+
+# typically i just include all of them for testing purposes
+# but you do you - I know how people get fired up about their browsers...
+# tbh I just main edge cause clean lines and Chromium that's better than Chrome
+# "fight! fight! fight!"
+$browsers = @(
+    "microsoft-edge"
+    "googlechrome"
+    "firefox"
+    )
+
+
+# not much - keep it simple.
+$entertainment = @(
+    "vlc"
+    "steam"
+    )
+
+
+Write-Host "Setting up runtimes..." -ForegroundColor Green
+InstallApps -Apps $langsAndRuntimes
+
+Write-Host "Installing Python and Configuring the enviornments..." -ForegroundColor Green
 Write-Host "------------------------------------" -ForegroundColor Green
 python -m pip install --upgrade pip
 python -m pip install requirements.txt
 mkdir git
+
+Write-Host "Grabbing browsers - hold tight and get ready to Google..." -ForegroundColor Green
+InstallApps -Apps $browsers
+
+Write-Host "Setting up editors..." -ForegroundColor Green
+InstallApps -Apps $editors
+
+Write-Host "Setting up utilities..." -ForegroundColor Green
+InstallApps -Apps $utilities
+
+
+Write-Host "Installing PC System applications..." -ForegroundColor Green
+InstallApps -Apps $pcSystemApps
+
+Write-Host "Productivity setup and install..." -ForegroundColor Green
+InstallApps -Apps $productivity
+
+Write-Host "Minimal entertainment! Installing the basics." -ForegroundColor Green
+InstallApps -Apps $entertainment
 
 Write-Host "------------------------------------" -ForegroundColor Green
 Read-Host -Prompt "Setup is done, restart is needed, press [ENTER] to restart computer."
