921f7352e82a08b830a3e12f8c5006cc02dabc42
theQuantumMechanic <albertferguson118@gmail.com>
2022-05-11 17:16:04 +1000

    configure system preferences in separate script

--- Install.ps1
+++ Install.ps1
@@ -7,20 +7,6 @@ function Check-Command($cmdname) {
     return [bool](Get-Command -Name $cmdname -ErrorAction SilentlyContinue)
 }
 
-# -----------------------------------------------------------------------------
-$computerName = Read-Host 'Enter New Computer Name'
-Write-Host "Renaming this computer to: " $computerName  -ForegroundColor Yellow
-Rename-Computer -NewName $computerName
-# -----------------------------------------------------------------------------
-Write-Host ""
-Write-Host "Disable Sleep on AC Power" -ForegroundColor Green
-Write-Host "------------------------------------" -ForegroundColor Green
-Powercfg /Change monitor-timeout-ac 10
-Powercfg /Change standby-timeout-ac 30
-Write-Host ""
-Write-Host "Enabling Dark Mode" -ForegroundColor Green
-Write-Host "------------------------------------" -ForegroundColor Green
-Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize -Name AppsUseLightTheme -Value 0
 
 # To list all appx packages:
 # Get-AppxPackage | Format-Table -Property Name,Version,PackageFullName
